f8909b6d1e46e4720f8465b02f5e93238eb1da96
angerypr <angerypr16@gmail.com>
2025-05-20 15:04:15 -0400

    Tarea 1 terminada.

---
+++ acerca_de.php
@@ -0,0 +1,50 @@
+<!-- Angery Payamps 2024-0215 -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Acerca de Angery</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+            text-align: center;
+            padding: 30px;
+        }
+
+        img {
+            border-radius: 10px;
+            margin: 20px 0;
+        }
+
+        a {
+            display: inline-block;
+            margin: 10px;
+            padding: 10px 15px;
+            background-color:rgb(255, 174, 0);
+            color: white;
+            text-decoration: none;
+            border-radius: 5px;
+            transition: background-color 0.3s;
+        }
+
+        a:hover {
+            background-color: rgb(179, 116, 0);
+        }
+
+        iframe {
+            margin-top: 30px;
+            max-width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <h2>Acerca De</h2>
+    <img src="IMG_2246.jpg" alt="Angery Payamps" height="200">
+    <h3>Nombre: Angery Payamps</h3>
+    <a href="https://t.me/angerypr" target="_blank">Telegram</a>
+    <a href="https://wa.me/18296997714" target="_blank">WhatsApp</a>
+    <br>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/-moW9jvvMr4" frameborder="0" allowfullscreen></iframe>
+</body>
+</html>
